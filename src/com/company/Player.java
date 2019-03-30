136334cefbcde7e616cbc474819f4af6ccc86b5c
Zach Burns <zachburns1@gmail.com>
2019-03-30 17:04:18 -0400

    player

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,56 @@
 package com.company;
-
+import java.util.*;
+// Health
+// Items
 public class Player {
+    private int health;
+    private String PlayerName;
+    private List<String> items;
+    private int currency;
+
+    public Player(){
+        health = 100;
+        PlayerName = "";
+        items = new ArrayList<String>();
+        currency = 0;
+    }
+
+    public void SetPlayer(String name){
+        PlayerName = name;
+    }
+
+    public String GetPlayer(){
+        return PlayerName;
+    }
+
+    public void SetHealth(int player_health){
+        health = player_health;
+    }
+
+    public int GetHealth(){
+        return health;
+    }
+
+    public List GetItems(){
+        return items;
+    }
+
+    public void PickupItem(String item_name) {
+        items.add(item_name);
+    }
+
+    public void DropItem(String item_name){
+        int size = items.size();
+
+        for (int i = 0; i < size; i++) {
+            if (item_name == items.get(i)){
+                items.remove(i);
+                break;
+            }
+        }
+    }
+
+
+
+
 }
