768c5f4a85c0e23c42b98dd289281fd01f105773
Kevin Zhang <kzh227@g.uky.edu>
2019-03-30 18:25:04 -0400

    quickUpdate

--- src/com/company/BattleSystem.java
+++ src/com/company/BattleSystem.java
@@ -1,13 +1,15 @@
 package com.company;
 
 public class BattleSystem {
+    private Thread t;
 
-    public void startBattle(Player p){
-
+    public void setUpBattle(Player p){
         BattleRunnable br = new BattleRunnable();
-        Thread t = new Thread(br);
+        t = new Thread(br);
+    }
+
+    public void startThread(){
         t.start();
-        
     }
 
     private Enemy pickEnemy(){
