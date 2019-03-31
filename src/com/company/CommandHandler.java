18b97e1393eddb08dd88fa5bee674ea5b41782e0
mthompson4 <mthompson2297@gmail.com>
2019-03-30 21:11:44 -0400

    Movement

--- src/com/company/CommandHandler.java
+++ src/com/company/CommandHandler.java
@@ -11,7 +11,7 @@ public class CommandHandler {
         for(int i = 0; i < 5; i++) {
             rooms.add(new ArrayList<>());
             for(int j = 0; j < 5; j++) {
-                rooms.get(i).add(new Room());
+                rooms.get(i).add(new Room(false));
             }
         }
 
@@ -19,44 +19,60 @@ public class CommandHandler {
         currentRoomY = 0;
 
         //Cottage
+        rooms.get(0).get(0).setRoomName("Cottage");
         rooms.get(0).get(0).setRoomMessage("You wake up in a small one-room cottage. You are laying on a short cot. There is a suit of armor hanging on the wall and a villager’s tunic spread out on a table. Which do you choose to put on?");
         rooms.get(0).get(0).setDesc("Its a cottage. It smells like cheese");
         rooms.get(0).get(0).setItems("testitem"); //REMOVE LATER
+        rooms.get(0).get(0).setInUse(true);
 
         //Central
+        rooms.get(0).get(1).setRoomName("Central Path");
         rooms.get(0).get(1).setRoomMessage("");
         rooms.get(0).get(1).setDesc("");
         rooms.get(0).get(1).setItems("");
+        rooms.get(0).get(1).setInUse(true);
 
         //Lake
+        rooms.get(0).get(2).setRoomName("Lake");
         rooms.get(0).get(2).setRoomMessage("");
         rooms.get(0).get(2).setDesc("");
         rooms.get(0).get(2).setItems("");
+        rooms.get(0).get(2).setInUse(true);
 
         //foggy path
+        rooms.get(1).get(1).setRoomName("Foggy Path");
         rooms.get(1).get(1).setRoomMessage("");
         rooms.get(1).get(1).setDesc("");
         rooms.get(1).get(1).setItems("");
+        rooms.get(1).get(1).setInUse(true);
 
         //forest
+        rooms.get(1).get(2).setRoomName("Forest");
         rooms.get(1).get(2).setRoomMessage("");
         rooms.get(1).get(2).setDesc("");
         rooms.get(1).get(2).setItems("");
+        rooms.get(1).get(2).setInUse(true);
 
         //fork
+        rooms.get(2).get(2).setRoomName("Fork in the Road");
         rooms.get(2).get(2).setRoomMessage("");
         rooms.get(2).get(2).setDesc("");
         rooms.get(2).get(2).setItems("");
+        rooms.get(2).get(2).setInUse(true);
 
         //goblin clearing
+        rooms.get(2).get(3).setRoomName("Clearing infested with Goblins!");
         rooms.get(2).get(3).setRoomMessage("");
         rooms.get(2).get(3).setDesc("");
         rooms.get(2).get(3).setItems("");
+        rooms.get(2).get(3).setInUse(true);
 
         //bat clearing
+        rooms.get(3).get(2).setRoomName("Clearing infested with Bats!");
         rooms.get(3).get(2).setRoomMessage("");
         rooms.get(3).get(2).setDesc("");
         rooms.get(3).get(2).setItems("");
+        rooms.get(3).get(2).setInUse(true);
 
     }
 
@@ -68,15 +84,50 @@ public class CommandHandler {
         return command;
     }
 
-    public void execute(String command){
+    public String execute(String command){
         switch(command){
 
             case("look"):
-                rooms.get(currentRoomX).get(currentRoomY).getDesc();
-                break;
+                return rooms.get(currentRoomX).get(currentRoomY).getDesc();
 
-            case("north"):
-                currentRoomY++;
+            case("move north"):
+                if((currentRoomY - 1) < 0) return "The path north is blocked.";
+                if(rooms.get(currentRoomX).get(currentRoomY-1).isInUse()) {
+                    currentRoomY--;
+                    return "You have moved north. You are now at a " + rooms.get(currentRoomX).get(currentRoomY).getRoomName();
+                }
+                else
+                    return "The path north is blocked.";
+
+            case("move west"):
+                if((currentRoomX - 1) < 0) return "The path west is blocked.";
+                if(rooms.get(currentRoomX-1).get(currentRoomY).isInUse()) {
+                    currentRoomX--;
+                    return "You have moved west. You are now at a " + rooms.get(currentRoomX).get(currentRoomY).getRoomName();
+                }
+                else
+                    return "The path west is blocked.";
+
+            case("move south"):
+                if((currentRoomY + 1) > (rooms.size() - 1)) return "The path south is blocked.";
+                if(rooms.get(currentRoomX).get(currentRoomY+1).isInUse()) {
+                    currentRoomY++;
+                    return "You have moved south. You are now at a " + rooms.get(currentRoomX).get(currentRoomY).getRoomName();
+                }
+                else
+                    return "The path south is blocked.";
+
+            case("move east"):
+                if((currentRoomX + 1) > (rooms.size() - 1)) return "The path east is blocked.";
+                if(rooms.get(currentRoomX+1).get(currentRoomY).isInUse()) {
+                    currentRoomX++;
+                    return "You have moved east. You are now at a " + rooms.get(currentRoomX).get(currentRoomY).getRoomName();
+                }
+                else
+                    return "The path east is blocked.";
+
+            default:
+                return "Unrecognized command.";
 
         }
 
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -3,37 +3,57 @@ import java.util.*;
 public class Room {
 
     private List<String> items;
-    private String RoomMessage;
-    private String Desc;
+    private String name;
+    private String roomMessage;
+    private String desc;
+    private boolean inUse;
 
-    public Room(){
-        RoomMessage = "";
-        Desc = "";
+    public Room(boolean b){
+        items = new ArrayList<>();
+        roomMessage = "";
+        desc = "";
+        name = "";
+        inUse = b;
+    }
+
+    public void setRoomName(String n) {
+        name = n;
+    }
+
+    public String getRoomName() {
+        return name;
     }
 
     public void setRoomMessage(String desc){
-        RoomMessage = desc;
+        roomMessage = desc;
     }
     public String getRoomMessage(){
-        return RoomMessage;
+        return roomMessage;
     }
 
     public void setDesc(String desc){
-        Desc = desc;
+        this.desc = desc;
     }
 
     public String getDesc(){
-        return Desc;
+        return desc;
     }
 
     public void setItems(String item){
         items.add(item);
     }
 
-    public List getItems(){
+    public List<String> getItems(){
         return items;
     }
 
+    public void setInUse(boolean b) {
+        inUse = b;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
 
 
 }
